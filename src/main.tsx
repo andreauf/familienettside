7092486aec068dc9d856afe066e9d6fb1e0ca78d
andreuf <andreuf@stud.ntnu.no>
2024-02-08 12:04:49 +0100

    fix problem

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { StrictMode } from "react";
 
-createRoot(document.getElementById("app")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const appElement = document.getElementById("app");
+
+if (appElement) {
+  createRoot(appElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error("Element with id 'app' not found in the document.");
+}
