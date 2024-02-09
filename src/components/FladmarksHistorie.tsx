1a8166cc66890415ac62cacf4b5ad34ab7301bb0
andreuf <andreuf@stud.ntnu.no>
2024-02-09 15:46:34 +0100

    Endret navn på sider og la til tekst skrevet av farfar

--- src/components/FladmarksHistorie.tsx
+++ src/components/FladmarksHistorie.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Byfolk } from "./Byfolk";
 
 export function Fladmark() {
   return (
     <>
-      <p>Her kommer Fladmarks historie</p>
+      <Byfolk />
     </>
   );
 }
